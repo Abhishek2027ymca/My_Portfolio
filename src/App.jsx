92fcfbff0fa7f805924e6375ceaa4ddc3d91a9cf
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-06-14 22:49:38 +0530

    effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
+import CustomCursor from './components/CustomCursor'
+import SectionDivider from './components/SectionDivider'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
@@ -11,17 +14,25 @@ import Contact from './sections/Contact'
 function App() {
   return (
     <div style={{ minHeight: '100vh', backgroundColor: '#05060A', color: '#F5F7FA' }}>
+      <CustomCursor />
       <Navbar />
       <main>
         <Hero />
+        <SectionDivider />
         <About />
+        <SectionDivider />
         <Projects />
+        <SectionDivider />
         <Skills />
+        <SectionDivider />
         <Experience />
+        <SectionDivider />
         <Achievements />
+        <SectionDivider />
         <Contact />
       </main>
       <Footer />
+      <ScrollToTop />
     </div>
   )
 }
